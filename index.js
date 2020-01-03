78a28403ba31f0505a5505d71fa7bea87ac74693
Vinicius Claudino <gnu.vinicius@gmail.com>
2020-01-03 15:58:44 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+if ( navigator.geolocation ) {
+    navigator.geolocation.getCurrentPosition(setCurrentPosition, positionError, {
+        enableHighAccuracy: false,
+        timeout: 15000,
+        maximumAge: 0
+    })
+}
+
+function setCurrentPosition(position) {
+    console.log(position)
+    document.querySelector( '.latitude' ).innerHTML = position.coords.latitude;
+    document.querySelector( '.longitude' ).innerHTML = position.coords.longitude;
+
+}
+
+function positionError( error ) {
+
+    switch ( error.code ) {
+        case error.PERMISSION_DENIED:
+            console.error( "User denied the request for Geolocation." );
+            document.querySelector( '.error' ).innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            console.error( "Location information is unavailable." );
+            break;
+        case error.TIMEOUT:
+            console.error( "The request to get user location timed out." );
+            break;
+        case error.UNKNOWN_ERROR:
+            console.error( "An unknown error occurred." );
+            break;
+    }
+
+}
